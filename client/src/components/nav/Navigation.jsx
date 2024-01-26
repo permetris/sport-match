b2a63fddb119439e2d42b423a9bdffd80357e22c
Josip Bogunovic <jboguno@gmail.com>
2024-01-26 16:50:55 +0100

    Refactor navigation

--- client/src/components/nav/Navigation.jsx
+++ client/src/components/nav/Navigation.jsx
@@ -2,20 +2,41 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { toastSuccess, toastError } from '../../hooks/useToastify';
+import { Link } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { isLoggedIn } from '../../utils/isLoggedIn';
+const sx = {
+  marginRight: 1
+};
 
 const Navigation = () => {
+  const navigate = useNavigate();
+  const onLogoutHandler = async (e) => {
+    try {
+      e.preventDefault();
+      localStorage.clear();
+      toastSuccess(successMessages.userLoggedOut);
+      navigate('/');
+    } catch (err) {
+      toastError(err);
+    }
+  };
   return (
-    <AppBar position="static">
-      <Toolbar>
+    <AppBar position="fixed" sx={{ paddingLeft: '27%', paddingRight: '27%' }}>
+      <Toolbar >
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           Sport Scheduler
         </Typography>
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">About</Button>
-        <Button color="inherit">Contact</Button>
+
+        <Link href='/'>Home</Link>
+        {!isLoggedIn() && <Link sx={sx} color="inherit" underline='hover' href='/login'>Log In</Link>}
+        {!isLoggedIn() && <Link sx={sx} color="inherit" underline='hover' href='/register'>Register</Link>}
+        {isLoggedIn() && <Link sx={sx} color="inherit" underline='hover' href='/user'>My Profile</Link>}
+        {isLoggedIn() && <Link sx={sx} color="inherit" underline='hover' href='/history'>Match History</Link>}
+        {isLoggedIn() && <Link sx={sx} color="inherit" underline='hover' onClick={onLogoutHandler} href='/'>Logout</Link>}
       </Toolbar>
-    </AppBar>
+    </AppBar >
   );
 };
 
